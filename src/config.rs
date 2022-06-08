e66993fd6044984830fa1021d98c84e4c1788b86
Simeon Ricking <simeon.ricking@rwth-aachen.de>
2022-06-08 23:50:18 +0200

    Replace INI config format with TOML
    Adjust the Config struct, its function Config::with_args() and add
    additional functions and structs in module config to change the parsed
    file formal from INI to TOML.
    Add the possibility to configure the server with multiple local
    addresses and multiple certificates corresponding to different
    domains.
    Change and rename the example config file to the new format.

--- src/config.rs
+++ src/config.rs
@@ -1,12 +1,15 @@
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::BufReader;
+use std::io::{BufReader, Read};
 use std::net::{SocketAddr, ToSocketAddrs};
 use std::path::PathBuf;
 use std::sync::Arc;
 
-use configparser::ini::Ini;
-use rustls::{server::ServerConfig, Certificate, PrivateKey};
+use rustls::{
+    server::{ClientHello, ResolvesServerCert, ServerConfig},
+    sign::CertifiedKey,
+    Certificate, PrivateKey,
+};
 use rustls_pemfile::{read_all, read_one, Item};
 use users::{get_group_by_name, get_user_by_name, Group, User};
 
@@ -39,55 +42,207 @@ impl Config {
         };
 
         // Load config file:
-        let mut file_cfg = Ini::new();
-        file_cfg
-            .load(&config_path)
-            .map_err(|s| Error::Config(format!("Could not parse INI file: {}", s)))?;
-
-        let mut main_section = file_cfg.remove_section("KUTSCHE").ok_or_else(|| {
-            Error::Config(format!(
-                "Missing section 'KUTSCHE' in config file {}.",
-                &config_path
-            ))
-        })?;
+        let mut cfg_file_buf = String::new();
+        let mut cfg_file = File::open(&config_path)?; // TODO: Make async
+        cfg_file.read_to_string(&mut cfg_file_buf)?;
+        let file_cfg = if let toml::Value::Table(map) = toml::from_str(cfg_file_buf.as_str())
+            .map_err(|e| Error::Config(format!("Could not parse config file: {}", e)))?
+        {
+            map
+        } else {
+            return Err(Error::Config(
+                "Could not parse config file: Root Value not a Table.".to_string(),
+            ));
+        };
 
         // Get local socket address or default:
-        let local_addrs: Vec<SocketAddr> = main_section
-            .remove("bind_address")
-            .flatten()
-            .unwrap_or_else(|| "127.0.0.1:25".to_string())
-            .to_socket_addrs()
-            .map_err(|_| {
-                Error::Config(
-                    "Could not resolve value of 'bind_address' in main section of config."
-                        .to_string(),
-                )
-            })?
-            .collect();
+        let local_addrs = match file_cfg.get("bind_addresses") {
+            Some(toml::Value::Array(addrs_list)) => {
+                let mut local_addrs = vec![];
+                for addr in addrs_list.iter() {
+                    if let toml::Value::String(addr) = addr {
+                        local_addrs.extend(addr.to_socket_addrs().map_err(|_| Error::Config("Could not resolve value of 'bind_address' in main section of config."
+                                .to_string()))?);
+                    } else {
+                        return Err(Error::Config("'bind_addresses' contains a value with wrong type (expected type string).".to_string()));
+                    }
+                }
+                local_addrs
+            }
+            Some(_) => {
+                return Err(Error::Config(
+                    "Field 'bind_addresses' has wrong type (should be of type Array).".to_string(),
+                ));
+            }
+            None => vec!["127.0.0.1:25"
+                .to_socket_addrs()
+                .expect("This should always work.")
+                .next()
+                .unwrap()],
+        };
 
         // Get new unix user and group:
-        let effective_user = if let Some(name) = main_section.remove("unix_user").flatten() {
-            Some(get_user_by_name(&name).ok_or_else(|| {
-                Error::Config("The user given by 'unix_user' does not exist.".to_string())
-            })?)
+        let effective_user = if let Some(name_val) = file_cfg.get("unix_user") {
+            Some(
+                get_user_by_name(name_val.as_str().ok_or_else(|| {
+                    Error::Config(
+                        "Value of field 'unix_user' has wrong type (expected string).".to_string(),
+                    )
+                })?)
+                .ok_or_else(|| {
+                    Error::Config("The user given by 'unix_user' does not exist.".to_string())
+                })?,
+            )
         } else {
             None
         };
-        let effective_group = if let Some(name) = main_section.remove("unix_group").flatten() {
-            Some(get_group_by_name(&name).ok_or_else(|| {
-                Error::Config("The group given by 'unix_group' does not exist.".to_string())
-            })?)
+        let effective_group = if let Some(name_val) = file_cfg.get("unix_group") {
+            Some(
+                get_group_by_name(name_val.as_str().ok_or_else(|| {
+                    Error::Config(
+                        "Value of field 'unix_group' has wrong type (expected string).".to_string(),
+                    )
+                })?)
+                .ok_or_else(|| {
+                    Error::Config("The group given by 'unix_group' does not exist.".to_string())
+                })?,
+            )
         } else {
             None
         };
 
         // Get TLS configuration:
         let tls_config = if local_addrs.iter().any(|addr| addr.port() == 465) {
+            let cert_section = file_cfg
+                .get("certificates")
+                .ok_or_else(|| {
+                    Error::Config("Missing 'certificates' section in config file.".to_string())
+                })?
+                .as_table()
+                .ok_or_else(|| {
+                    Error::Config(
+                        "Wrong type of 'certificate' section in config file (expected table)."
+                            .to_string(),
+                    )
+                })?;
+
+            Some(TlsConfig::try_from(cert_section)?.into())
+        } else {
+            None
+        };
+
+        // Get default file destination base directory:
+        let default_path: Option<PathBuf> = if let Some(val) = file_cfg.get("default_path") {
+            Some(PathBuf::from(val.as_str().ok_or_else(|| {
+                Error::Config(
+                    "Value of field 'default_path' has wrong type (expected string).".to_string(),
+                )
+            })?))
+        } else {
+            None
+        };
+
+        Config {
+            effective_user,
+            effective_group,
+            local_addrs,
+            default_path,
+            dest_map: HashMap::new(),
+            tls_config,
+        }
+        .load_mapping(
+            file_cfg
+                .get("mappings")
+                .ok_or_else(|| {
+                    Error::Config("Missing 'mappings' sections in config file.".to_string())
+                })?
+                .as_table()
+                .ok_or_else(|| {
+                    Error::Config(
+                        "Wrong type of 'mappings' section in config file (expected table)."
+                            .to_string(),
+                    )
+                })?,
+        )
+    }
+
+    /// Loads a destination mapping from the given mappings sections from the config file to the own field dest_map.
+    fn load_mapping(
+        mut self,
+        mapping_sections: &toml::map::Map<String, toml::Value>,
+    ) -> Result<Self, Error> {
+        for mapping_name in mapping_sections.keys() {
+            let map_section = mapping_sections
+                .get(mapping_name)
+                .unwrap() // Cannor be None, because mapping_name name is in mapping_sections.keys().
+                .as_table()
+                .ok_or_else(|| {
+                    Error::Config(format!(
+                        "Section 'mappings.{}' has wrong type (expected table).",
+                        mapping_name
+                    ))
+                })?;
+
+            let addr_key = map_section
+                .get("address")
+                .ok_or_else(|| Error::Config(format!("Mapping {} is missing 'address' field.", mapping_name)))?
+                .as_str()
+                .ok_or_else(|| {
+                    Error::Config(format!("Field 'address' for mapping '{mapping_name}' has wrong type (expected string)."))
+                })?;
+
+            let dest_value = if let Some(path) = map_section.get("dest_path") {
+                FileDestination::new(path.as_str().ok_or_else(|| Error::Config(format!("Field 'dest_path' for mapping '{mapping_name}' has wrong type (expected string).")))?)
+            } else if let Some(ref base_path) = self.default_path {
+                let mut path = PathBuf::from(base_path);
+                path.push(&addr_key);
+                FileDestination::new(path)
+            } else {
+                Err(Error::Config(format!(
+                    "Missing destination for mapping '{mapping_name}'."
+                )))
+            }?;
+
+            self.dest_map
+                .insert(String::from(addr_key), Box::new(dest_value));
+        }
+
+        Ok(self)
+    }
+}
+
+// We only use this struct to circumvent rusts rules for implementing foreign traits on foreign types.
+// We cannot directly implement TryFrom<toml::map::Map<String, toml::Value>> for ServerConfig.
+struct TlsConfig(ServerConfig);
+impl From<TlsConfig> for Arc<ServerConfig> {
+    fn from(conf: TlsConfig) -> Self {
+        Arc::new(conf.0)
+    }
+}
+impl TryFrom<&toml::map::Map<String, toml::Value>> for TlsConfig {
+    type Error = Error;
+
+    fn try_from(cert_section: &toml::map::Map<String, toml::Value>) -> Result<Self, Self::Error> {
+        let mut resolver = CertResolver::new();
+
+        for domain in cert_section.keys() {
+            // Get configured paths:
+            let domain_cert_obj = cert_section[domain]
+				.as_table()
+				.ok_or_else(|| Error::Config(format!("Value for domain {} in 'certificates' section has wrong type (expected table).", domain)))?;
+            let cert_file_path = domain_cert_obj
+				.get("cert_file")
+				.ok_or_else(|| Error::Config(format!("Missing field 'cert_file' for domain {}.", domain)))?
+				.as_str()
+				.ok_or_else(|| Error::Config(format!("Value for field 'cert_file' for domain {} in 'certificates' section has wrong type (expected string).", domain)))?;
+            let key_file_path = domain_cert_obj
+				.get("private_key_file")
+				.ok_or_else(|| Error::Config(format!("Missing field 'private_key_file' for domain {}.", domain)))?
+				.as_str()
+				.ok_or_else(|| Error::Config(format!("Value for field 'private_key_file' for domain {} in 'certificates' section has wrong type (expected string).", domain)))?;
+
             // Read certificates:
-            let cert_file =
-                File::open(main_section.remove("cert_file").flatten().ok_or_else(|| {
-                    Error::Config("Missing key 'cert_file' in main section of config.".to_string())
-                })?)?;
+            let cert_file = File::open(cert_file_path)?;
             let mut reader = BufReader::new(cert_file);
             let certs = read_all(&mut reader)?
                 .into_iter()
@@ -101,79 +256,63 @@ impl Config {
                 .collect();
 
             // Read private key:
-            let key_file_path = main_section
-                .remove("private_key_file")
-                .flatten()
-                .ok_or_else(|| {
-                    Error::Config(
-                        "Missing key 'private_key_file' in main section of config.".to_string(),
-                    )
-                })?;
             let key_file = File::open(&key_file_path)?;
             let mut reader = BufReader::new(key_file);
-            let priv_key = if let Some(Item::RSAKey(raw) | Item::PKCS8Key(raw) | Item::ECKey(raw)) =
-                read_one(&mut reader)?
-            {
-                PrivateKey(raw)
-            } else {
-                return Err(Error::Config(format!(
-                    "Could not read key from {} given by 'private_key_file'.",
-                    key_file_path
-                )));
-            };
+            let priv_key_signer =
+                if let Some(Item::RSAKey(raw) | Item::PKCS8Key(raw) | Item::ECKey(raw)) =
+                    read_one(&mut reader)?
+                {
+                    rustls::sign::any_supported_type(&PrivateKey(raw)).map_err(|e| {
+                        Error::Config(format!(
+                            "Could not sign with private key given for domain {}: {}",
+                            domain, e
+                        ))
+                    })?
+                } else {
+                    return Err(Error::Config(format!(
+                        "Could not read key from {} given by 'private_key_file'.",
+                        key_file_path
+                    )));
+                };
 
-            Some(Arc::new(
-                ServerConfig::builder()
-                    .with_safe_defaults()
-                    .with_no_client_auth()
-                    .with_single_cert(certs, priv_key)?,
-            ))
-        } else {
-            None
-        };
-
-        // Get default file destination base directory:
-        let default_path: Option<PathBuf> = main_section
-            .remove("default_path")
-            .flatten()
-            .map(PathBuf::from);
-
-        Config {
-            effective_user,
-            effective_group,
-            local_addrs,
-            default_path,
-            dest_map: HashMap::new(),
-            tls_config,
+            resolver.add_domain(
+                domain.to_string(),
+                CertifiedKey::new(certs, priv_key_signer),
+            );
         }
-        .load_mapping(file_cfg)
+
+        Ok(Self(
+            ServerConfig::builder()
+                .with_safe_defaults()
+                .with_no_client_auth()
+                .with_cert_resolver(Arc::new(resolver)),
+        ))
+    }
+}
+
+pub(crate) struct CertResolver {
+    domain_cert_map: HashMap<String, Arc<CertifiedKey>>,
+}
+
+impl CertResolver {
+    fn new() -> Self {
+        CertResolver {
+            domain_cert_map: HashMap::new(),
+        }
     }
 
-    /// Loads a destination mapping from the given INI file representation to the own field dest_map.
-    fn load_mapping(mut self, mapping_config: Ini) -> Result<Self, Error> {
-        for mapping_name in mapping_config.sections() {
-            let addr_key = mapping_config
-                .get(mapping_name.as_str(), "address")
-                .ok_or_else(|| {
-                    Error::Config(format!("Missing 'address' for mapping '{mapping_name}'."))
-                })?;
-            let dest_value =
-                if let Some(path) = mapping_config.get(mapping_name.as_str(), "dest_path") {
-                    FileDestination::new(path)
-                } else if let Some(ref base_path) = self.default_path {
-                    let mut path = PathBuf::from(base_path);
-                    path.push(&addr_key);
-                    FileDestination::new(path)
-                } else {
-                    Err(Error::Config(format!(
-                        "Missing destination for mapping '{mapping_name}'."
-                    )))
-                }?;
+    fn add_domain(&mut self, domain: String, cert: CertifiedKey) {
+        self.domain_cert_map.insert(domain, Arc::new(cert));
+    }
+}
 
-            self.dest_map.insert(addr_key, Box::new(dest_value));
+impl ResolvesServerCert for CertResolver {
+    fn resolve(&self, client_hello: ClientHello) -> Option<Arc<CertifiedKey>> {
+        if let Some(domain) = client_hello.server_name() {
+            self.domain_cert_map.get(domain).cloned()
+        } else {
+            None
         }
-
-        Ok(self)
     }
 }
 
